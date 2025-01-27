8137a8fe71f3450781c9b06fa1d7edd4e7849970
Antoine-Bottin <a.bottin.dev@gmail.com>
2025-01-27 15:37:16 +0100

    feat(page): under construction view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-    return <div>Coucou</div>
+import { ReactElement } from 'react'
+import Image from 'next/image'
+
+const Page = (): ReactElement => {
+    return (
+        <div className="flex flex-col items-center content-center -rotate-[30deg]">
+            <Image
+                src="/images/under-construction.png"
+                width={500}
+                height={500}
+                alt="Site web under construction"
+            />
+            <div className="text-4xl">Website under construction...</div>
+        </div>
+    )
 }
+
+export default Page
