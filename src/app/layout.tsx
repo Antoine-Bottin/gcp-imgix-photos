b423cd4e327cd2d8cb7911f47f5dbdb1a36ca6fe
Antoine-Bottin <a.bottin.dev@gmail.com>
2025-01-29 15:48:40 +0100

    feat(setup): remove google signin occurences

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} flex justify-center items-center h-screen`}
             >
-                {children}
                 <SpeedInsights />
+                {children}
             </body>
         </html>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
+'use client'
+
 import { ReactElement } from 'react'
-import Image from 'next/image'
 
 const Page = (): ReactElement => {
     return (
-        <div className="flex flex-col items-center content-center -rotate-[30deg]">
-            <Image
-                src="/images/under-construction.png"
-                width={500}
-                height={500}
-                alt="Site web under construction"
-            />
-            <div className="text-4xl">Website under construction...</div>
+        <div className="flex flex-col items-center content-center">
+            <h2 className="mb-10">Website under construction</h2>
         </div>
     )
 }
 
 export default Page
-
-//ftp.abottin.dev.  0	CNAME	abottin.dev.
-
-//www.abottin.dev.  0	  A	    213.186.33.5
